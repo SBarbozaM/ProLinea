df645cb8e2d3e925f58c5affdc58522e75e488a8
SBarbozaM <sbarboza@linea.pe>
2026-02-18 18:07:13 -0500

    face id 1

--- lib/src/utils/app_data.dart
+++ lib/src/utils/app_data.dart
@@ -1,8 +1,8 @@
 import 'package:geolocator/geolocator.dart';
 
 class AppData {
-  static const String appVersion = '4.5.1+55';
-  static const String appFechaCompilacion = '18/02/2026 15:30';
+  static const String appVersion = '4.5.1+56';
+  static const String appFechaCompilacion = '18/02/2026 18:00';
   static const int radioGeocerca = 2;
   static const accuracy = LocationAccuracy.bestForNavigation;
 }
