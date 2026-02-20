d6c41a6c3ac60421e3d2340f94a5dd3356fd9d12
SBarbozaM <sbarboza@linea.pe>
2026-02-20 12:25:54 -0500

    deshabilitar ver dscto 1

--- lib/src/utils/app_data.dart
+++ lib/src/utils/app_data.dart
@@ -1,8 +1,8 @@
 import 'package:geolocator/geolocator.dart';
 
 class AppData {
-  static const String appVersion = '4.5.1+59';
-  static const String appFechaCompilacion = '20/02/2026 11:00';
+  static const String appVersion = '4.5.1+60';
+  static const String appFechaCompilacion = '20/02/2026 12:30';
   static const int radioGeocerca = 2;
   static const accuracy = LocationAccuracy.bestForNavigation;
 }
