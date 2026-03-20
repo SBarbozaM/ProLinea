011fe632f558a72d776aa942c1907a0554f66e7c
SBarbozaM <sbarboza@linea.pe>
2026-03-20 12:13:27 -0500

    ajustes a manifiestos version

--- lib/src/utils/app_data.dart
+++ lib/src/utils/app_data.dart
@@ -1,8 +1,8 @@
 import 'package:geolocator/geolocator.dart';
 
 class AppData {
-  static const String appVersion = '4.6.1+63';
-  static const String appFechaCompilacion = '20/03/2026 10:07';
+  static const String appVersion = '4.6.1+66';
+  static const String appFechaCompilacion = '20/03/2026 12:13';
   static const int radioGeocerca = 2;
   static const accuracy = LocationAccuracy.bestForNavigation;
   static const apiSeguridad = 'https://linea.pe/api_security/v4';
